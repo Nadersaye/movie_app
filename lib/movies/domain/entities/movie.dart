0d93fb724b233b1bc5737ba5136ac00c8756a137
Nader <ns1568@fayoum.edu.eg>
2024-01-24 22:14:17 +0200

    Refactor Movie class to make it immutable and implement Equatable

--- lib/movies/domain/entities/movie.dart
+++ lib/movies/domain/entities/movie.dart
@@ -1,5 +1,6 @@
-// i want to make this class immutable, so i use final keyword
-class Movie {
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
   final int id;
   final String title;
   final List<int> genreIds;
@@ -7,13 +8,27 @@ class Movie {
   final String overview;
   final String posterPath;
   final double voteAverage;
+  final String releaseDate;
 
-  Movie(
+  const Movie(
       {required this.id,
       required this.title,
       required this.genreIds,
       required this.backdropPath,
       required this.overview,
       required this.posterPath,
-      required this.voteAverage});
+      required this.voteAverage,
+      required this.releaseDate});
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        genreIds,
+        backdropPath,
+        overview,
+        posterPath,
+        voteAverage,
+        releaseDate
+      ];
 }
