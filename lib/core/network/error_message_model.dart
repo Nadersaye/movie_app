9652020352c19fb87c7fda1d81e7e457021d378c
Nader <ns1568@fayoum.edu.eg>
2024-01-25 13:19:27 +0200

    Add ErrorMessageModel class to handle network error messages

---
+++ lib/core/network/error_message_model.dart
@@ -0,0 +1,15 @@
+class ErrorMessageModel {
+  final String statusMessage;
+  final int statusCode;
+  final bool success;
+
+  ErrorMessageModel(
+      {required this.statusMessage,
+      required this.statusCode,
+      required this.success});
+  factory ErrorMessageModel.fromJson(Map<String, dynamic> json) =>
+      ErrorMessageModel(
+          statusMessage: json["status_message"],
+          statusCode: json["status_code"],
+          success: json["success"]);
+}
