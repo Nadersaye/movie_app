8c06cb7004e9e07c622ac9ab4c8ad5d4a9001d5e
Nader <ns1568@fayoum.edu.eg>
2024-01-25 13:19:53 +0200

    Add API constants for movie endpoints

---
+++ lib/core/network/api_constances.dart
@@ -0,0 +1,9 @@
+class ApiConstants {
+  static const String baseUrl = "https://api.themoviedb.org/3/";
+  static const String apiKey = "8d99e7a13f7817fa58a887e894adf899";
+  static const String accessToken =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDk5ZTdhMTNmNzgxN2ZhNThhODg3ZTg5NGFkZjg5OSIsInN1YiI6IjY1YWY5NzZhMzk3NTYxMDBhZmQwN2U1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.80rlzxZ69FQP8FVuhez0ngJoHwah4ujCBQNgbFL_pTg";
+  static const String nowPlayingMoviesUrl = "${baseUrl}movie/now_playing";
+  static const String popularMoviesUrl = "${baseUrl}movie/popular";
+  static const String topRatedMoviesUrl = "${baseUrl}movie/top_rated";
+}
