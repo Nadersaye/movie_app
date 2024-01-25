f00e0ee9c1478e7aea50f7c624e56f584006160e
Nader <ns1568@fayoum.edu.eg>
2024-01-25 13:20:28 +0200

    Add MovieModel class

---
+++ lib/movies/data/models/movie_model.dart
@@ -0,0 +1,23 @@
+import 'package:movie_app/movies/domain/entities/movie.dart';
+
+class MovieModel extends Movie {
+  const MovieModel(
+      {required super.id,
+      required super.title,
+      required super.genreIds,
+      required super.backdropPath,
+      required super.overview,
+      required super.posterPath,
+      required super.voteAverage,
+      required super.releaseDate});
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
+      id: json["id"],
+      title: json["title"],
+      genreIds: List<int>.from(json["genre_ids"].map((e) => e)),
+      backdropPath: json["backdrop_path"],
+      overview: json["overview"],
+      posterPath: json["poster_path"],
+      voteAverage: json["vote_average"],
+      releaseDate: json["release_date"]);
+}
