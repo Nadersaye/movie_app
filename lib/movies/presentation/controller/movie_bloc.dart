45cc7c45d038a6afc1ae9defe83c205f4ad7e045
Nader <ns1568@fayoum.edu.eg>
2024-01-27 04:00:46 +0200

    Add popular and top rated movies use cases to MovieBloc

--- lib/movies/presentation/controller/movie_bloc.dart
+++ lib/movies/presentation/controller/movie_bloc.dart
@@ -3,19 +3,49 @@ import 'package:movie_app/movies/domain/usecases/get_now_playing_movies_usecase.
 import 'package:movie_app/movies/presentation/controller/movie_event.dart';
 import 'package:movie_app/movies/presentation/controller/movie_state.dart';
 import '../../../core/utils/request_state_enum.dart';
+import '../../domain/usecases/get_popular_movies_usecase.dart';
+import '../../domain/usecases/get_top_rated_movies_usecase.dart';
 
 class MovieBloc extends Bloc<MovieEvent, MoviesState> {
   final GetNowPlayingMoviesUsecase getNowPlayingMoviesUsecase;
-  MovieBloc(this.getNowPlayingMoviesUsecase) : super(const MoviesState()) {
+  final GetPopularMoviesUsecase getPopularMoviesUsecase;
+  final GetTopRatedMoviesUsecase getTopRatedMoviesUsecase;
+  MovieBloc(this.getNowPlayingMoviesUsecase, this.getPopularMoviesUsecase,
+      this.getTopRatedMoviesUsecase)
+      : super(const MoviesState()) {
     on<GetNowPlayingMoviesEvent>((event, emit) async {
       final resualt = await getNowPlayingMoviesUsecase.execute();
       resualt.fold((l) {
-        emit(MoviesState(
+        emit(state.copyWith(
             nowPlayingMoviesState: RequestState.error,
             nowPlayingMoviesErrorMessage: l.errorMessage));
       }, (r) {
-        emit(MoviesState(
-            nowPlayingMoviesState: RequestState.loaded, nowPlayingMovies: r));
+        emit(state.copyWith(
+            nowPlayingMovies: r, nowPlayingMoviesState: RequestState.loaded));
+      });
+    });
+
+    on<GetPopularMoviesEvent>((event, emit) async {
+      final resualt = await getPopularMoviesUsecase.execute();
+      resualt.fold((l) {
+        emit(state.copyWith(
+            popularMoviesState: RequestState.error,
+            popularMoviesErrorMessage: l.errorMessage));
+      }, (r) {
+        emit(state.copyWith(
+            popularMovies: r, popularMoviesState: RequestState.loaded));
+      });
+    });
+
+    on<GetTopRatedMoviesEvent>((event, emit) async {
+      final resualt = await getTopRatedMoviesUsecase.execute();
+      resualt.fold((l) {
+        emit(state.copyWith(
+            topRatedMoviesState: RequestState.error,
+            topRatedMoviesErrorMessage: l.errorMessage));
+      }, (r) {
+        emit(state.copyWith(
+            topRatedMovies: r, topRatedMoviesState: RequestState.loaded));
       });
     });
   }
