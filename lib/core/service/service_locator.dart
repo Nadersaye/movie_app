8da286f9c085dcb0b09a8e660f09b59f39b6615a
Nader <ns1568@fayoum.edu.eg>
2024-01-27 03:58:27 +0200

    Add new movie use cases and update service locator

--- lib/core/service/service_locator.dart
+++ lib/core/service/service_locator.dart
@@ -1,19 +1,22 @@
 import 'package:get_it/get_it.dart';
 import 'package:movie_app/movies/domain/repository/base_movies_repository.dart';
 import 'package:movie_app/movies/domain/usecases/get_now_playing_movies_usecase.dart';
+import 'package:movie_app/movies/domain/usecases/get_top_rated_movies_usecase.dart';
 import '../../movies/data/datasource/movie_remote_data_source.dart';
 import '../../movies/data/repository/movies_repository.dart';
+import '../../movies/domain/usecases/get_popular_movies_usecase.dart';
 import '../../movies/presentation/controller/movie_bloc.dart';
 
 final sl = GetIt.instance;
 
 class ServiceLocator {
   static void init() {
-    sl.registerFactory(() => MovieBloc(sl()));
+    sl.registerFactory(() => MovieBloc(sl(), sl(), sl()));
 
     sl.registerLazySingleton(
         () => GetNowPlayingMoviesUsecase(repository: sl()));
-
+    sl.registerLazySingleton(() => GetPopularMoviesUsecase(repository: sl()));
+    sl.registerLazySingleton(() => GetTopRatedMoviesUsecase(repository: sl()));
     sl.registerLazySingleton<BaseMoviesRemoteDataSource>(
         () => MovieRemoteDataSource());
 
