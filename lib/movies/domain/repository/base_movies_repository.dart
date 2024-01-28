e99e32b62d61486412b92a3e92c41eaa142e7b01
Nader <ns1568@fayoum.edu.eg>
2024-01-28 17:39:59 +0200

    Add movie details and recommendations to base movies repository

--- lib/movies/domain/repository/base_movies_repository.dart
+++ lib/movies/domain/repository/base_movies_repository.dart
@@ -1,9 +1,17 @@
 import 'package:dartz/dartz.dart';
+import 'package:movie_app/movies/domain/entities/movie_details.dart';
+import 'package:movie_app/movies/domain/entities/recommendation_movie.dart';
+import 'package:movie_app/movies/domain/usecases/get_movie_details_usecase.dart';
 import '../../../core/error/failure.dart';
 import '../entities/movie.dart';
+import '../usecases/get_recommendation_movies_usecase.dart';
 
 abstract class BaseMoviesRepository {
   Future<Either<Failure, List<Movie>>> getNowPlayingMovies();
   Future<Either<Failure, List<Movie>>> getPopularMovies();
   Future<Either<Failure, List<Movie>>> getTopRatedMovies();
+  Future<Either<Failure, MovieDetails>> getMovieDetails(
+      MovieDetailsParameters parameters);
+  Future<Either<Failure, List<RecommendationsMovie>>> getRecommendationMovies(
+      RecommendationMoviesParameters parameters);
 }
