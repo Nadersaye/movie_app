0d4bb41326a467a6479a8a76f5a18a8bcc6c9bed
Nader <ns1568@fayoum.edu.eg>
2024-01-28 17:44:50 +0200

    Refactor GetPopularMoviesUsecase to extend BaseUseCase

--- lib/movies/domain/usecases/get_popular_movies_usecase.dart
+++ lib/movies/domain/usecases/get_popular_movies_usecase.dart
@@ -1,15 +1,16 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../core/error/failure.dart';
+import '../../../core/usecase/base_usecase.dart';
 import '../entities/movie.dart';
 import '../repository/base_movies_repository.dart';
 
-class GetPopularMoviesUsecase {
+class GetPopularMoviesUsecase extends BaseUseCase<List<Movie>, NoParameters> {
   final BaseMoviesRepository repository;
 
   GetPopularMoviesUsecase({required this.repository});
-
-  Future<Either<Failure, List<Movie>>> execute() async {
+  @override
+  Future<Either<Failure, List<Movie>>> call(NoParameters paremater) async {
     return await repository.getPopularMovies();
   }
 }
