c1eaec816a0074116aacfb993bbea5c1a3bae683
Nader <ns1568@fayoum.edu.eg>
2024-01-28 17:44:28 +0200

    Refactor GetNowPlayingMoviesUsecase to extend BaseUseCase

--- lib/movies/domain/usecases/get_now_playing_movies_usecase.dart
+++ lib/movies/domain/usecases/get_now_playing_movies_usecase.dart
@@ -1,14 +1,16 @@
 import 'package:dartz/dartz.dart';
 import '../../../core/error/failure.dart';
+import '../../../core/usecase/base_usecase.dart';
 import '../entities/movie.dart';
 import '../repository/base_movies_repository.dart';
 
-class GetNowPlayingMoviesUsecase {
+class GetNowPlayingMoviesUsecase
+    extends BaseUseCase<List<Movie>, NoParameters> {
   final BaseMoviesRepository repository;
 
   GetNowPlayingMoviesUsecase({required this.repository});
-
-  Future<Either<Failure, List<Movie>>> execute() async {
+  @override
+  Future<Either<Failure, List<Movie>>> call(NoParameters paremater) async {
     return await repository.getNowPlayingMovies();
   }
 }
