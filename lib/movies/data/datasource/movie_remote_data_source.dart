3a32218eb807be5e2659f27878b43cf7055eb1e3
Nader <ns1568@fayoum.edu.eg>
2024-01-28 17:34:19 +0200

    Add movie details and recommendation methods to MovieRemoteDataSource

--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -1,14 +1,22 @@
 import 'package:dio/dio.dart';
 import 'package:movie_app/core/error/exception.dart';
 import 'package:movie_app/core/network/api_constances.dart';
+import 'package:movie_app/movies/data/models/movie_details_model.dart';
 import 'package:movie_app/movies/data/models/movie_model.dart';
+import 'package:movie_app/movies/data/models/recommendation_movie_model.dart';
+import 'package:movie_app/movies/domain/usecases/get_movie_details_usecase.dart';
 
 import '../../../core/network/error_message_model.dart';
+import '../../domain/usecases/get_similar_movies_usecase.dart';
 
 abstract class BaseMoviesRemoteDataSource {
   Future<List<MovieModel>> getNowPlayingMovies();
   Future<List<MovieModel>> getPopularMovies();
   Future<List<MovieModel>> getTopRatedMovies();
+
+  Future<MovieDetailsModel> getMovieDetails(MovieDetailsParameters parameters);
+  Future<List<RecommnedationMovieModel>> getRecommendationMovies(
+      RecommendationMoviesParameters parameters);
 }
 
 class MovieRemoteDataSource implements BaseMoviesRemoteDataSource {
@@ -18,8 +26,8 @@ class MovieRemoteDataSource implements BaseMoviesRemoteDataSource {
         options: Options(
             contentType: 'application/json',
             headers: {"Authorization": "Bearer ${ApiConstance.accessToken}"}));
-    print('hkjh');
     if (responce.statusCode == 200) {
+      print('playing');
       /*
       return List<MovieModel>.from(
         (responce.data['results'] as List).map((e) => MovieModel.fromJson(e)));
@@ -39,6 +47,7 @@ class MovieRemoteDataSource implements BaseMoviesRemoteDataSource {
             contentType: 'application/json',
             headers: {"Authorization": "Bearer ${ApiConstance.accessToken}"}));
     if (responce.statusCode == 200) {
+      print('popular');
       return List<MovieModel>.from((responce.data['results'] as List)
           .map((e) => MovieModel.fromJson(e)));
     } else {
@@ -54,8 +63,45 @@ class MovieRemoteDataSource implements BaseMoviesRemoteDataSource {
             contentType: 'application/json',
             headers: {"Authorization": "Bearer ${ApiConstance.accessToken}"}));
     if (responce.statusCode == 200) {
+      print('top rated');
       final data = responce.data['resualts'] as List;
       return data.map((e) => MovieModel.fromJson(e)).toList();
+    } else {
+      throw ServerException(
+          errorMessageModel: ErrorMessageModel.fromJson(responce.data));
+    }
+  }
+
+  @override
+  Future<MovieDetailsModel> getMovieDetails(
+      MovieDetailsParameters parameters) async {
+    final responce = await Dio().get(
+        ApiConstance.movieDetailsUrl(parameters.movieId),
+        options: Options(
+            contentType: 'application/json',
+            headers: {"Authorization": "Bearer ${ApiConstance.accessToken}"}));
+    if (responce.statusCode == 200) {
+      print('top rated');
+      final data = responce.data['resualts'];
+      return MovieDetailsModel.fromJson(data);
+    } else {
+      throw ServerException(
+          errorMessageModel: ErrorMessageModel.fromJson(responce.data));
+    }
+  }
+
+  @override
+  Future<List<RecommnedationMovieModel>> getRecommendationMovies(
+      RecommendationMoviesParameters parameters) async {
+    final responce = await Dio().get(
+        ApiConstance.movieRecommendationsUrl(parameters.movieId),
+        options: Options(
+            contentType: 'application/json',
+            headers: {"Authorization": "Bearer ${ApiConstance.accessToken}"}));
+    if (responce.statusCode == 200) {
+      print('top rated');
+      final data = responce.data['resualts'] as List;
+      return data.map((e) => RecommnedationMovieModel.fromJson(e)).toList();
     } else {
       throw ServerException(
           errorMessageModel: ErrorMessageModel.fromJson(responce.data));
