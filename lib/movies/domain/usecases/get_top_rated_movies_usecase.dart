3964ab6bc3231f8b6324c3ee1d9bf66eabcf2497
Nader <ns1568@fayoum.edu.eg>
2024-01-28 17:44:38 +0200

    Refactor GetTopRatedMoviesUsecase to extend BaseUseCase

--- lib/movies/domain/usecases/get_top_rated_movies_usecase.dart
+++ lib/movies/domain/usecases/get_top_rated_movies_usecase.dart
@@ -1,14 +1,15 @@
 import 'package:dartz/dartz.dart';
 import '../../../core/error/failure.dart';
+import '../../../core/usecase/base_usecase.dart';
 import '../entities/movie.dart';
 import '../repository/base_movies_repository.dart';
 
-class GetTopRatedMoviesUsecase {
+class GetTopRatedMoviesUsecase extends BaseUseCase<List<Movie>, NoParameters> {
   final BaseMoviesRepository repository;
 
   GetTopRatedMoviesUsecase({required this.repository});
-
-  Future<Either<Failure, List<Movie>>> execute() async {
+  @override
+  Future<Either<Failure, List<Movie>>> call(NoParameters paremater) async {
     return await repository.getTopRatedMovies();
   }
 }
