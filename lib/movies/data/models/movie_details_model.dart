298265dec95b8aa5f1ac99a4b1337ab1f1558623
Nader <ns1568@fayoum.edu.eg>
2024-01-28 17:35:49 +0200

    Add MovieDetailsModel to handle movie details data

---
+++ lib/movies/data/models/movie_details_model.dart
@@ -0,0 +1,27 @@
+import 'package:movie_app/movies/data/models/genres_model.dart';
+
+import '../../domain/entities/movie_details.dart';
+
+class MovieDetailsModel extends MovieDetails {
+  const MovieDetailsModel(
+      {required super.id,
+      required super.title,
+      required super.genres,
+      required super.backdropPath,
+      required super.overview,
+      required super.releaseDate,
+      required super.voteAverage,
+      required super.runtime});
+
+  factory MovieDetailsModel.fromJson(Map<String, dynamic> json) =>
+      MovieDetailsModel(
+          id: json['id'],
+          title: json['title'],
+          genres:
+              List.from(json['genres'].map((e) => GeneresModel.fromJson(e))),
+          backdropPath: json['backdrop_path'],
+          overview: json['overview'],
+          releaseDate: json['release_date'],
+          voteAverage: json['vote_average'],
+          runtime: json['runtime']);
+}
